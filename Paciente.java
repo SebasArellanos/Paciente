39ae987f9086e7a21ab464254f97aa5643afb75a
Sebastian Lozada <sebaslozaare@gmail.com>
2024-04-25 22:42:07 -0600

    version 1

---
+++ Paciente.java
@@ -0,0 +1,16 @@
+class Paciente {
+    String nombre, apellido;
+    int nss;
+
+    public void mostrarNombre() {
+        System.out.println("Minombre es: " + this.nombre);
+    }
+
+    public void mostrarApellido() {
+        System.out.println("Mi apellido es: " + this.apellido);
+    }
+
+    public void mostrarNss() {
+        System.out.println("Mi nss es: " + this.nss);
+    }
+}
