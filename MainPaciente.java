bc05fe8352ff47dc48b76537a7f1dfe9d7993db9
Sebastian Lozada <sebaslozaare@gmail.com>
2024-05-02 23:32:24 -0600

    version 4

--- MainPaciente.java
+++ MainPaciente.java
@@ -4,22 +4,22 @@ class MainPaciente {
 
     public static void main(String[] args) {
         Scanner op = new Scanner(System.in);
-        String a, b;
-        int c;
+        String nombre, apellido;
+        int nss;
         Paciente x = new Paciente();
         System.out.println("cual es tu nombre");
-        a = op.next();
-        x.nombre = a;
-        System.out.println("cual es tu apellido");
-        b = op.next();
-        x.apellido = b;
-        System.out.println("cual es tu nss");
-        c = op.nextInt();
-        x.nss = c;
+        nombre = op.next();
+        x.setNombre(nombre);
 
-        x.mostrarNombre();
-        x.mostrarApellido();
-        x.mostrarNss();
+        System.out.println("cual es tu apellido");
+        apellido = op.next();
+        x.setApellido(apellido);
+
+        System.out.println("cual es tu nss");
+        nss = op.nextInt();
+        x.setNss(nss);
+
+        System.out.println(x);
 
     }
 
--- Paciente.java
+++ Paciente.java
@@ -1,6 +1,40 @@
 class Paciente {
-    String nombre, apellido;
-    int nss;
+    private String nombre, apellido;
+    private int nss;
+
+    @Override
+    public String toString() {
+        return "Paciente [nombre=" + nombre + ", apellido=" + apellido + ", nss=" + nss + "]";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNss() {
+        return nss;
+    }
+
+    public void setNss(int nss) {
+        if (nss > 0) {
+            this.nss = nss;
+        } else {
+            System.out.println("nss invalido");
+        }
+
+    }
 
     public void mostrarNombre() {
         System.out.println("Mi nombre es: " + this.nombre);
